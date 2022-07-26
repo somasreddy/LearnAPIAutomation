8fca08a010a2c5916182e073c2c8bcb809ca23a3
V, Somasekhar <Somasekhar.V@cerner.com>
2022-07-26 10:16:08 +0530

    updated the comments

--- src/main/java/API_Tests/Static_Test2.java
+++ src/main/java/API_Tests/Static_Test2.java
@@ -8,9 +8,10 @@ import org.testng.annotations.Test;
 public class Static_Test2 {
 	@Test
 	public void test() {
-		baseURI = "https://reqres.in/api/";
+		baseURI = "https://reqres.in/api/"; //is the base url
+	// we can use this as we have static imported the restassured and Matchers library   
 		given().
-			get("users?page=2").
+			get("users?page=2"). //end point of the api
 		then().
 			statusCode(200).
 			body("data[1].id", equalTo(8)).
